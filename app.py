7485f2fdb1f64c833f471e581df4eef19c90f036
Syed Abdul Qadir Gilani <66156655+SyedAbdulQadirGilani001@users.noreply.github.com>
2023-07-03 16:49:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,30 @@
-import streamlit as st # Streamlit for the web app
-import pandas as pd # Pandas for data manipulation
-from sklearn import datasets # Scikit-learn for machine learning
-from sklearn.ensemble import RandomForestClassifier # Random forest classifier
-
-st.write("""  
-# Simple Iris Flower Prediction App 
-This app predicts the **Iris flower** type! 
-""") # Markdown for the web app
-st.sidebar.header('User Input Parameters') # Sidebar for user input parameters
-
-def user_input_features(): # Function for user input features
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4) # Slider for sepal length
-    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4) # Slider for sepal width
-    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3) # Slider for petal length
-    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2) # Slider for petal width
-    data = {'sepal_length': sepal_length, # Dictionary for user input features
-            'sepal_width': sepal_width, # Dictionary for user input features
-            'petal_length': petal_length, # Dictionary for user input features
-            'petal_width': petal_width}
-    features = pd.DataFrame(data, index=[0]) # Dataframe for user input features
-    return features # Return user input features
-
-df = user_input_features() # User input features
-
-st.subheader('User Input parameters') # Subheader for user input parameters
-st.write(df) # Display user input parameters
-
-iris = datasets.load_iris() # Load iris dataset
-X = iris.data # Features
-Y = iris.target # Target variable
-
-clf = RandomForestClassifier() # Random forest classifier
-clf.fit(X, Y) # Fit the model
-
-prediction = clf.predict(df) # Prediction
-prediction_proba = clf.predict_proba(df) # Prediction probability
-
-st.subheader('Class labels and their corresponding index number') # Subheader for class labels and their corresponding index number
-st.write(iris.target_names) # Display class labels and their corresponding index number
-
-st.subheader('Prediction') # Subheader for prediction
-st.write(iris.target_names[prediction]) # Display prediction
-#st.write(prediction)
-
-st.subheader('Prediction Probability') # Subheader for prediction probability
-st.write(prediction_proba)  # Display prediction probability
+# import liberaries 
+import streamlit as st # for web app, front end
+import plotly.express as px # for ploting, graphing
+import pandas as pd # for data manipulation, data analysis
+# Importing the dataset
+st.title("Gapminder Data Visualization")
+df=px.data.gapminder() # to load the data
+# show top 10 countries with highest population uniquely
+df=df.sort_values(by="pop",ascending=False) # sort the data by population
+df=df.drop_duplicates(subset="country",keep="first") # drop duplicate countries
+# show the data
+st.dataframe(df.head(10)) # show the data in table format
+# summary statistics
+st.write(df.describe())
+# data management
+year_option=df["year"].unique().tolist() # get the unique years
+# year=st.selectbox("Select Year",year_option,0) # create a drop down menu
+# filter the data
+# df=df[df["year"]==year] # filter the data by year
+fig=px.scatter(df,x="gdpPercap",y="lifeExp",size="pop",color="continent",
+               hover_name="continent",log_x=True,size_max=55
+               ,range_x=[100,100000],range_y=[20,90]
+                ,animation_frame="year",animation_group="country",
+                labels=dict(pop="Population",gdpPercap="GDP Per Capita",lifeExp="Life Expectancy")
+                ,title="Gapminder Data Visualization",template="plotly_dark"
+                ,color_continuous_scale=px.colors.sequential.Viridis
+                ,hover_data=["continent"]) # create the plot
+fig.layout.updatemenus[0].buttons[0].args[1]["frame"]["duration"]=3000 # set the animation speed
+fig.update_layout(width=800,height=600) # set the width and height of the plot
+st.plotly_chart(fig) # show the plot
